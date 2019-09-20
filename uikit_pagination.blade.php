d2d359ebbf75fee3d5d9c22988c69d42b9ae5564
PIERO <jenabi.pirooz@gmail.com>
2019-09-20 17:15:35 +0430

    Create uikit_pagination.blade.php
    add to view folder in laravel

---
+++ uikit_pagination.blade.php
@@ -0,0 +1,15 @@
+@if ($paginator->lastPage() > 1)
+<ul class="uk-pagination uk-flex-center" uk-margin>
+    <li class="{{ ($paginator->currentPage() == 1) ? ' uk-disabled' : '' }}">
+        <a href="{{ $paginator->url($paginator->currentPage()-1) }}"> <span uk-pagination-previous></span> </a>
+    </li>
+    @for ($i = 1; $i <= $paginator->lastPage(); $i++)
+        <li class="{{ ($paginator->currentPage() == $i) ? ' uk-active' : '' }}">
+            <a href="{{ $paginator->url($i) }}">{{ $i }}</a>
+        </li>
+    @endfor
+    <li class="{{ ($paginator->currentPage() == $paginator->lastPage()) ? ' uk-disabled' : '' }}">
+        <a href="{{ $paginator->url($paginator->currentPage()+1) }}" ><span uk-pagination-next></span></a>
+    </li>
+</ul>
+@endif
